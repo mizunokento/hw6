8a8ed11fc93c93eb15a965785f1314634dbfb29f
mizunokento <kmizuno@cca.edu>
2017-03-04 15:36:06 -0800

    Create lewitt_086.js

---
+++ lewitt_086.js
@@ -0,0 +1,8 @@
+function setup() { 
+  createCanvas(600, 400);
+	background(220);
+	stroke(150);
+	strokeWeight(.5);
+	for(i=0;i<=100;i++){
+	line(random(-100,700),random(-100,700),random(-100,700),random(-100,700) );}
+} 

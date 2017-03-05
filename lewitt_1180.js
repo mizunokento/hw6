d2fa078ac57b9e3ca981c9fc9e195a7797935894
mizunokento <kmizuno@cca.edu>
2017-03-04 17:00:17 -0800

    Create lewitt_1180.js

---
+++ lewitt_1180.js
@@ -0,0 +1,14 @@
+function setup() { 
+  createCanvas(600, 400);
+	background(250);
+	stroke(0);
+	strokeWeight(.5);
+	for(i=0;i<=15000;i++){
+	
+line(random(-1600,1400),random(-1600,1400),random(-1600,1400),
+		random(-1600,1400) );}
+} 
+
+function draw() { 
+  
+}
